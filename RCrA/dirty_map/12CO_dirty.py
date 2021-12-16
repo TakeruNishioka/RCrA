bac79b0c705a0334d0ce9e511327e8f138982949
Takeru Nishioka <tnishioka@a.phys.nagoya-u.ac.jp>
2021-12-16 17:20:05 +0900

    write code of making 12CO dirtymap

---
+++ RCrA/dirty_map/12CO_dirty.py
@@ -0,0 +1,51 @@
+import datetime
+import os
+
+time_start = datetime.datetime.now()
+print("CLEAN is Running")
+
+# Define CLEAN parameters
+vis = "RCrA_12CO_all.ms"
+prename = "RCrA_12CO_all"
+imsize = [1600, 1200]
+cell = "1arcsec"
+minpb = 0.2
+restfreq = "230.538GHz"
+outframe = "lsrk"
+spw = "0~35"
+width = "0.2km/s"
+start = "-25km/s"
+nchan = 300
+robust = 0.5
+phasecenter = "J2000 19h01m41 -36d57m00"
+
+# Make initial dirty image
+os.system("rm -rf dirty/" + prename + "_dirty.*")
+tclean(
+    vis=vis,
+    imagename="dirty/" + prename + "_dirty",
+    gridder="mosaic",
+    pbmask=minpb,
+    imsize=imsize,
+    cell=cell,
+    spw=spw,
+    weighting="briggs",
+    robust=robust,
+    phasecenter=phasecenter,
+    specmode="cube",
+    restfreq=restfreq,
+    width=width,
+    start=start,
+    nchan=nchan,
+    outframe=outframe,
+    veltype="radio",
+    restoringbeam="common",
+    mask="",
+    niter=0,
+    interactive=False,
+)
+
+time_finish = datetime.datetime.now()
+print("END CLEAN")
+print("start time : " + str(time_start))
+print("finish time : " + str(time_finish))
